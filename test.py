5efbeae2a159d8d46b94cd7dc1dee0bfd56b3b00
edi ekeng <86800363+Edi360@users.noreply.github.com>
2025-01-25 16:13:05 -0500

    tester.py was reacted
    created the tester.py file. within it is a function which asks the user for input to print a word a swell as ask the user for input to on how many times they should print P.

---
+++ test.py
@@ -0,0 +1,12 @@
+
+
+def first_function():
+    a = input('what would you like to print: ')
+    print (a)
+
+    b= int(input("how many time should i print P: "))
+
+    for i in range(b):
+        print (i,": P")
+
+first_function()
